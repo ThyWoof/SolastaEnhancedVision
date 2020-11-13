1c1e2029d930df9fc79765626d38273aaf8b50c4
Paulo Monteiro <pauloesquilo@gmail.com>
2020-11-12 19:41:09 -0800

    first commit

---
+++ SolastaEnhancedVision/Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityModManagerNet;
+using HarmonyLib;
+using System.Reflection;
+
+namespace SolastaEnhancedVision
+{
+    public class Main
+    {
+        [System.Diagnostics.Conditional("DEBUG")]
+        public static void Log(string msg)
+        {
+            if (logger != null) logger.Log(msg);
+        }
+        public static void Error(Exception ex)
+        {
+            if (logger != null) logger.Error(ex.ToString());
+        }
+        public static void Error(string msg)
+        {
+            if (logger != null) logger.Error(msg);
+        }
+
+        public static UnityModManager.ModEntry.ModLogger logger;
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+                var harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Error(ex);
+                throw ex;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(MainMenuScreen), "RuntimeLoaded")]
+        static class MainMenuScreen_Load_Patch
+        {
+            static void Postfix()
+            {
+                try
+                {
+                    var human = DatabaseRepository.GetDatabase<CharacterRaceDefinition>().GetElement("Human");
+                    var senseSuperiorDarkvision = DatabaseRepository.GetDatabase<FeatureDefinition>().GetElement("SenseSuperiorDarkvision");
+                    var featureUnlockSenseSuperiorDarkvision = new FeatureUnlockByLevel(senseSuperiorDarkvision, 1);
+                    human.FeatureUnlocks.Add(featureUnlockSenseSuperiorDarkvision);
+                }
+                catch (Exception ex)
+                {
+                    Error(ex);
+                    throw ex;
+                }
+
+            }
+        }
+    }
+}
